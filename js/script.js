6bbbdde51065818c33bbca99e6f6776790d679ab
M Michalczyk <pan.michalczyk@gmail.com>
2018-09-23 21:19:35 +0200

    Add gallery and map to Secret Places. Combine gallery with map

--- js/script.js
+++ js/script.js
@@ -34,4 +34,66 @@ function toggleMenu() {
         }
 }
 
+// Secret Places - Gallery
+const dataSlides = [
+    {
+        coords: {lat: -7.440434, lng: 34.895823},
+        description: '<img src="images/secret-places/savanna.jpeg" alt="Savanna, Africa">'
+    },
+    {
+        coords: {lat: 36.109017, lng: -112.148929},
+        description: '<img src="images/secret-places/canyon.jpeg" alt="Canyon, USA">'
+    },
+    {
+        coords: {lat: 53.549630, lng: 21.605829},
+        description: '<img src="images/secret-places/lake.jpeg" alt="Lake, Poland">'
+    }
+];
+
+// HTML TEMPLATE
+(function () {
+    const carouselGallery = document.getElementById('carouselSlide').innerHTML;
     
+    Mustache.parse(carouselGallery);
+    
+    let listGallery = '';
+    
+    for (let i = 0; i < dataSlides.length; i++) {
+        listGallery += Mustache.render(carouselGallery, dataSlides[i]);
+    } 
+    lolek.insertAdjacentHTML('beforeend', listGallery);
+})();
+
+// CAROUSEL
+const elem = document.querySelector('.main-carousel');
+const flkty = new Flickity( elem, {
+    // options
+    cellAlign: 'left',
+    wrapAround: true,
+    contain: true,
+    pageDots: false,
+    hash: true
+});
+
+// MAP
+window.initMap = function() {
+	
+    const map = new google.maps.Map(document.querySelector('#map'), {
+        zoom: 10,
+		center: dataSlides[0].coords
+    });
+
+	for (let i=0; i < dataSlides.length; i++ ){
+		const marker = new google.maps.Marker({
+			position: dataSlides[i].coords,
+			map: map
+		})
+    	marker.addListener('click', function(){
+			flkty.selectCell(i);
+        })
+	}
+    flkty.on( 'change', function( index ) {
+        map.panTo(dataSlides[index].coords); 
+        map.setZoom(7);
+    });
+}
